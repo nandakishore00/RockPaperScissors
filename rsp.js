418d75d412549bc11447b120781fbe303009669d
Nanda Kishore <nandakishorereddykamatham@gmail.com>
2023-01-12 14:30:07 +0530

    add output statements to the html page

--- rsp.js
+++ rsp.js
@@ -1,23 +1,32 @@
 let result=0
 let computerScore=0
 let playerScore=0
+
 let buttons=document.querySelectorAll('button')
+let output=document.querySelector('.output')
 function playRound(playerSelection,computerSelection) {
+    
     const ps=playerSelection.toUpperCase();
-    const cs=computerSelection.toUpperCase()
+    const cs=computerSelection.toUpperCase();
     if(ps=== "ROCK" && cs==="PAPER" ||ps=== "PAPER" && cs==="SCISSORS"||ps=== "SCISSORS" && cs==="ROCK"||ps=== "SCISSORS" && cs==="ROCK"){
         computerScore+=1
-        return `You loose! computer chose ${cs}`
+        let p=document.createElement('p')
+        p.innerText=`You loose! computer chose ${cs}`
+        output.appendChild(p)
     }
     else if(ps==cs)
     {
-        return `It's a Tie Round u chose ${ps}`
+        let p=document.createElement('p')
+        p.innerText=`It's a Tie Round u chose ${ps}`
+        output.appendChild(p)
     }
     else{
         playerScore+=1
-        return `You won! cp chose ${cs}`
-        
+        let p=document.createElement('p')
+        p.innerText=`You won! cp chose ${cs}`
+        output.appendChild(p)
     }
+    
     
   }
 let options=["rock","paper","scissors"]
