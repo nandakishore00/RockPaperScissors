3d4c3fe22b72ce8aa25b906b620106cf9a42ca94
Nanda Kishore <nandakishorereddykamatham@gmail.com>
2023-01-12 13:34:43 +0530

    add rock,paper,scissors buttons

--- rsp.js
+++ rsp.js
@@ -1,20 +1,22 @@
 let result=0
-let csr=0
-let=psr=0
+let computerScore=0
+let playerScore=0
+let buttons=document.querySelectorAll('button')
 function playRound(playerSelection,computerSelection) {
     const ps=playerSelection.toUpperCase();
     const cs=computerSelection.toUpperCase()
     if(ps=== "ROCK" && cs==="PAPER" ||ps=== "PAPER" && cs==="SCISSORS"||ps=== "SCISSORS" && cs==="ROCK"||ps=== "SCISSORS" && cs==="ROCK"){
-        csr+=1
-        return `You loose! ${cs} beat ${ps}`
+        computerScore+=1
+        return `You loose! computer chose ${cs}`
     }
     else if(ps==cs)
     {
-        return "It's a tie puta"
+        return `It's a Tie Round u chose ${ps}`
     }
     else{
+        playerScore+=1
         return `You won! cp chose ${cs}`
-        psr+=1
+        
     }
     
   }
@@ -23,25 +25,23 @@ function getComputerChoice(){
     return options[Math.floor(Math.random()*3)]
 }
 function game(){
-    for(let i=0;i<5; i++){
-        
-        let computerSelection= getComputerChoice();
-        let playerSelection=prompt("Choose Rock paper Scissors");
-        
-         console.log(playRound(playerSelection,computerSelection))
-
-        
-    }
-    console.log("Game over")
-    if(psr>csr){
-        result="player won"
-    }
-    else if(csr>psr){
-        result="Computer Won"
-    }
-    else{
-        result="its a tie"
-    }
-    return result
-} 
-console.log(game())
+    buttons.forEach(button=>{
+        button.addEventListener("click",function(){
+            let playerSelection=`${(this.textContent)}`
+            let computerSelection= getComputerChoice();
+            playRound(playerSelection,computerSelection)
+        })  
+    })
+}
+//console.log("Game over")
+//if(psr>csr){
+//    result=`Player wins ${psr} rounds and computer wins ${csr} rounds`
+//}
+//else if(csr>psr){
+//    result=`computer wins ${csr} rounds and Player wins ${psr} rounds`
+//}
+//else{
+//    result="its a tie"
+//}
+//return result
+(game())
